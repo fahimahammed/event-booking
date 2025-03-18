49259e1b7b7f0127bb653ef7514f861985e7126d
Fahim Ahammed Firoz <fahimahammed.cse@gmail.com>
2025-03-18 22:17:25 +0600

    refactor: modularize route handlers and improve code structure

--- main.go
+++ main.go
@@ -2,9 +2,7 @@ package main
 
 import (
 	"event-booking/db"
-	"event-booking/models"
-	"net/http"
-	"strconv"
+	"event-booking/routes"
 
 	"github.com/gin-gonic/gin"
 )
@@ -13,46 +11,7 @@ func main() {
 	db.InitDB()
 	server := gin.Default()
 
-	server.GET("/events", getEvents)
-	server.GET("/events/:id", getEvent)
-	server.POST("/events", createEvent)
+	routes.RegisterRoutes(server)
 
 	server.Run(":8080") // localhost:8080
-}
-
-func getEvents(context *gin.Context) {
-	events, err := models.GetAllEvents()
-	if err != nil {
-		context.JSON(http.StatusInternalServerError, gin.H{"error": err.Error(), "message": "Failed to get events"})
-		return
-	}
-	context.JSON(http.StatusOK, events)
-}
-
-func getEvent(context *gin.Context) {
-	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
-	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error(), "message": "Invalid event ID"})
-		return
-	}
-	event, err := models.GetEventById(eventId)
-	if err != nil {
-		context.JSON(http.StatusNotFound, gin.H{"error": err.Error(), "message": "Event not found"})
-		return
-	}
-	context.JSON(http.StatusOK, event)
-}
-
-func createEvent(context *gin.Context) {
-	var event models.Event
-	err := context.ShouldBindJSON(&event)
-
-	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error(), "message": "Invalid request body"})
-	}
-
-	event.ID = 1
-	event.UserID = 1
-	event.Save()
-	context.JSON(http.StatusCreated, gin.H{"message": "Event created successfully", "event": event})
 }
---
+++ routes/events.go
@@ -0,0 +1,46 @@
+package routes
+
+import (
+	"event-booking/models"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+func getEvents(context *gin.Context) {
+	events, err := models.GetAllEvents()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"error": err.Error(), "message": "Failed to get events"})
+		return
+	}
+	context.JSON(http.StatusOK, events)
+}
+
+func getEvent(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error(), "message": "Invalid event ID"})
+		return
+	}
+	event, err := models.GetEventById(eventId)
+	if err != nil {
+		context.JSON(http.StatusNotFound, gin.H{"error": err.Error(), "message": "Event not found"})
+		return
+	}
+	context.JSON(http.StatusOK, event)
+}
+
+func createEvent(context *gin.Context) {
+	var event models.Event
+	err := context.ShouldBindJSON(&event)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error(), "message": "Invalid request body"})
+	}
+
+	event.ID = 1
+	event.UserID = 1
+	event.Save()
+	context.JSON(http.StatusCreated, gin.H{"message": "Event created successfully", "event": event})
+}
