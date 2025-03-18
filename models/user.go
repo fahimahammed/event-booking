1fd44994f5dde78308c7d8d23367465029895915
Fahim Ahammed Firoz <fahimahammed.cse@gmail.com>
2025-03-19 00:07:11 +0600

    feat(login): add login functionality

--- models/user.go
+++ models/user.go
@@ -1,8 +1,10 @@
 package models
 
 import (
+	"errors"
 	"event-booking/db"
 	"event-booking/utils"
+	"fmt"
 )
 
 type User struct {
@@ -25,13 +27,38 @@ func (u User) Save() error {
 	if err != nil {
 		return err
 	}
-	u.Password = hashedPassword
+	fmt.Println("Hash Pass: ", hashedPassword)
 
-	result, err := stmt.Exec(u.Email, u.Password)
+	result, err := stmt.Exec(u.Email, hashedPassword)
 	if err != nil {
 		return err
 	}
 	id, err := result.LastInsertId()
+	if err != nil {
+		return err
+	}
 	u.ID = int64(id)
-	return err
+	return nil
+}
+
+func (u User) Login() error {
+	query := "SELECT password FROM users WHERE email = ?"
+	row := db.DB.QueryRow(query, u.Email)
+
+	var retrivedPassword string
+
+	err := row.Scan(&retrivedPassword)
+
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(retrivedPassword, u.Password)
+	passwordValid := utils.CheckPasswordHash(u.Password, retrivedPassword)
+
+	if !passwordValid {
+		return errors.New("invalid password")
+	}
+
+	return nil
 }
--- routes/users.go
+++ routes/users.go
@@ -13,7 +13,25 @@ func signup(context *gin.Context) {
 		context.JSON(400, gin.H{"error": err.Error()})
 		return
 	}
-
+	user.ID = 1
 	user.Save()
 	context.JSON(201, gin.H{"message": "User created successfully", "user": user})
+}
+
+func login(context *gin.Context) {
+	var user models.User
+	err := context.ShouldBindJSON(&user)
+	if err != nil {
+		context.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	err = user.Login()
+
+	if err != nil {
+		context.JSON(401, gin.H{"message": "Invalid credentials", "error": err})
+		return
+	}
+
+	context.JSON(200, gin.H{"message": "Login successful"})
 }
